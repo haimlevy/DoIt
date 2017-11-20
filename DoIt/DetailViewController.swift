a14f554ca782008fc887fdc537c87232a554a254
Haim Levy <haimlevi@gmail.com>
2017-11-20 23:11:58 +0200

    Added ability to edit and save todo

--- DoIt/DetailViewController.swift
+++ DoIt/DetailViewController.swift
@@ -10,19 +10,42 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    // Vars
     var isEdit : Bool = false;
     
+    // Outlets
+    @IBOutlet weak var txtFieldTitle: UITextField!;
+    @IBOutlet weak var txtViewNotes: UITextView!;
+    @IBOutlet weak var dtPickerDueDate: UIDatePicker!;
+    @IBOutlet weak var switchHasDueDate: UISwitch!;
+    
+    
     @IBAction func handleEdit(_ sender: UIBarButtonItem) {
-        self.txtFieldTitle.isEnabled = true;
+        // self.txtFieldTitle.isEnabled = true;
         
-        if isEdit
+        if (isEdit)
         {
             isEdit = false
             sender.title = "Edit"
             
-            // Disable UI and save the edited todo info
+            // Diable UI
             txtFieldTitle.isEnabled = false;
+            txtViewNotes.isEditable = false;
+            dtPickerDueDate.isEnabled = false;
+            switchHasDueDate.isEnabled = false;
+            
+            // Save the edited todo 
             detailItem?.title = txtFieldTitle.text!;
+            detailItem?.notes = txtViewNotes.text;
+            detailItem?.hasDueDate = switchHasDueDate.isOn
+            
+            if (switchHasDueDate.isOn) {
+                detailItem?.dueDate = dtPickerDueDate.date;
+            }
+            else {
+                detailItem?.dueDate = nil;
+            }
+
         }
         else
         {
@@ -31,33 +54,41 @@ class DetailViewController: UIViewController {
             
             // Enable UI to add ability to edit todo info
             txtFieldTitle.isEnabled = true;
+            txtViewNotes.isEditable = true;
+            dtPickerDueDate.isEnabled = true;
+            switchHasDueDate.isEnabled = true;
+            
         }
     }
-
-    @IBOutlet weak var txtFieldTitle: UITextField!
     
     func configureView() {
-//        // Update the user interface for the detail item.
-                if let detail = detailItem {
-                    if let txt1 = txtFieldTitle {
-        
-                    txt1.text = detail.title;
-                    // txtViewNotes.text = detail.notes;
-        
-                        // label.text = detail.description
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let tField = txtFieldTitle {
+                tField.text = detail.title;
+            }
+            if let txtViewNotes = self.txtViewNotes {
+                if let notes = detail.notes {
+                    txtViewNotes.text = notes;
                 }
+            }
+            if let switchHasDueDate = self.switchHasDueDate {
+                if let hasDueDate = detail.hasDueDate {
+                    switchHasDueDate.isOn = hasDueDate;
+                }
+            }
+
+            if let dtPickerDueDate = self.dtPickerDueDate {
+                if let dueDate = detail.dueDate {
+                    dtPickerDueDate.date = dueDate;
+            }
+
+        }
         
-//                    if let txt2 = txtViewNotes {
-//
-//                        txt2.text = detail.notes;
-//                        // txtViewNotes.text = detail.notes;
-//
-//                        // label.text = detail.description
-//                    }
-                }}
-    
-    
-    
+        }
+        
+    }
+     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
